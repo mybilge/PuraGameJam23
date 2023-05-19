ad9bed8001115891a70b77c9e9ef1acfc78356d8
mybilge <mehmetyusufbilge@gmail.com>
2023-05-20 00:09:24 +0300

    easter egg eklendi

--- Assets/Scripts/EasterEgg.cs
+++ Assets/Scripts/EasterEgg.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EasterEgg : MonoBehaviour
 {
@@ -9,21 +10,49 @@ public class EasterEgg : MonoBehaviour
 
     [SerializeField] List<Sprite> easterEggSprites;
     [SerializeField] Image easterEggImage;
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Button showBtn;
+
+    int sayac = 0;
+
+    private void Start() {
+        clickCount = 0;
+        easterEggImage.enabled = false;
+        showBtn.enabled = false;
+        inputField.text = "";
+    }
+
+
     public void EasterEggOnClick()
     {
         clickCount++;
 
-        if(clickCount >= 5)
+        if(clickCount >= 1)
         {
-            easterEggImage.enabled = true;
-            easterEggImage.sprite = easterEggSprites[Random.Range(0, easterEggSprites.Count)];
+            Show();
             clickCount = 0;
         }
+    }
+
+    public void CheckText()
+    {
+        if(inputField.text == "minnosum")
+        {
+            showBtn.enabled = true;
+        }
+    }
+
+    void Show()
+    {
+        easterEggImage.enabled = true;
+        easterEggImage.sprite = easterEggSprites[(sayac++)%easterEggSprites.Count];
     }
 
 
     private void OnEnable() {
         clickCount = 0;
         easterEggImage.enabled = false;
+        showBtn.enabled = false;
+        inputField.text = "";
     }
 }
